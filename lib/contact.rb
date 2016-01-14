d195dc0d91e66a9612205746dac6ff51eee8df88
Kyle Langley and Mike Mahoney <altck8@gmail.com>
2016-01-14 08:44:15 -0800

    add mailing address class with attributes

--- lib/contact.rb
+++ lib/contact.rb
@@ -1,9 +1,25 @@
 class Contact
-  attr_reader(:name, :phone, :email, :relationship)
+  attr_reader(:first_name, :last_name, :job_title, :company)
   define_method(:initialize) do |attributes|
-    @name = attributes.fetch("name")
-    @phone = attributes.fetch("phone")
-    @email = attributes.fetch("email")
-    @relationship = attributes.fetch("relationship")
+    @first_name = attributes.fetch("first_name")
+    @last_name = attributes.fetch("last_name")
+    @job_title = attributes.fetch("job_title")
+    @company = attributes.fetch("company")
   end
 end
+
+
+=begin
+
+Start by making a Contact class with properties such as first name, last name,
+job title, company. Each contact can have multiple mailing addresses, phone
+numbers and email addresses. Create classes for each of these, one at a time.
+
+Allow users to add mailing addresses for each contact with a street address,
+city, state and zip. Also, include a type (home, office, etc).
+Then, allow users to select a contact, and add phone numbers. Each phone number
+ should have an area code, a number, and a type, like "home" or "cell".
+Now, allow users to add multiple emails for their contacts with an address and
+type.
+
+=end
--- spec/contact_spec.rb
+++ spec/contact_spec.rb
@@ -1,14 +1,34 @@
 require 'rspec'
 require 'contact'
+require 'mailing_address'
 
 describe Contact do
   describe "#contact_list" do
     it 'shows the contact list saved in contacts' do
-      new_contact = Contact.new({"name" => "friend", "phone" => 0000000000, "email" => "email@email.com", "relationship" => "co-worker"})
-      expect(new_contact.name).to(eq("friend"))
-      expect(new_contact.phone).to(eq(0000000000))
-      expect(new_contact.email).to(eq("email@email.com"))
-      expect(new_contact.relationship).to(eq("co-worker"))
+      new_contact = Contact.new({ "first_name" => "bob",
+                                  "last_name" => "jim",
+                                  "job_title" => "manager",
+                                  "company" => "water sports inc"})
+
+      expect(new_contact.first_name).to(eq("bob"))
+      expect(new_contact.last_name).to(eq("jim"))
+      expect(new_contact.job_title).to(eq("manager"))
+      expect(new_contact.company).to(eq("water sports inc"))
+    end
+  end
+end
+
+describe Mailing_Address do
+  describe "#mailing_address" do
+    it 'creates a new mailing list' do
+      mailing = Mailing_Address.new({"street_address" => "400 SW 6th",
+                                    "city" => "Portland",
+                                    "state" => "Oregon",
+                                    "zip" => 97219 })
+      expect(mailing.street_address).to(eq("400 SW 6th"))
+      expect(mailing.city).to(eq("Portland"))
+      expect(mailing.state).to(eq("Oregon"))
+      expect(mailing.zip).to(eq(97219))
     end
   end
 end
